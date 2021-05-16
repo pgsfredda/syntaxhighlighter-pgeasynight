32b5420876ca5da90c194e8f13b68404ee17c6f7
Paolo Gabriele Sfredda <paolo@Amsterdam-2.local>
2021-05-16 12:12:58 +0200

    First commit

---
+++ syntaxhighlighter-pgeasynight.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: Syntax Tomorrow
+ * Plugin URI: http://wordpress.org/plugins/
+ * Description: Tomorrow Theme for the SyntaxHighligher Evolved plugin
+ * Author: P.G.Sfredda
+ * Version: 1.0.0
+ * Author URI: https://paologabrielesfredda.it https://creativaweb.it
+ * License: MIT
+ */
+
+/**
+ * Learn how to build your own syntaxhighlighter theme here:
+ * http://www.viper007bond.com/wordpress-plugins/syntaxhighlighter/adding-a-new-theme/
+ */
+
+/**
+ * Register our new stylesheet, on the site and in the admin.
+ */
+function pg_register_easynight_style() {
+	wp_register_style( 'syntaxhighlighter-theme-pgeasynight',
+		plugins_url( 'pgEasyNight.css' , __FILE__ ),
+		array( 'syntaxhighlighter-core' ),
+		'1.0.0'
+	);
+}
+add_action( 'wp_register_style', 'pg_register_easynight_style' );
+
+/**
+ * Add that new stylesheet as an option for the SyntaxHighligher Evolved plugin.
+ *
+ * @param array $themes Array of SyntaxHighligther themes.
+ */
+function pg_new_syntax_theme( $themes ) {
+	$themes['pgeasynight'] = 'pg Easy Night';
+
+	return $themes;
+}
+add_filter( 'syntaxhighlighter_themes', 'pg_new_syntax_theme' );
